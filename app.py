f67710ad58d7300aa1dd04c69167c6a72dc5b358
ngathu2020 <github@norths.com>
2018-04-24 19:40:58 -0500

    uploaded app.py

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ def home():
     return send_from_directory('', 'index.html')
 
 
+@app.route('/<path:path>')
+def send_static_file(path):
+    return send_from_directory('', path)
+
+
 
 if __name__ == "__main__":
   app.run(debug=True)

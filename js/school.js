a0b4a4bb484451c84a9855a70c44818c6faa5958
CGeorge3 <33561937+CGeorge3@users.noreply.github.com>
2018-04-29 20:52:13 -0500

    Update school.js

--- js/school.js
+++ js/school.js
@@ -29,51 +29,10 @@ d3.csv("MoDataSet.csv", function (error, MoDataSet) {
     }
     renderTable()
 })
-$searchBtn.addEventListener("click", handleSearchButtonClick);
-function handleSearchButtonClick() {
 
-    filteredMissouridata = MoDataSet;
-    var filterArray = [{
-        "key": "district",
-        "value": $districtInput.value
-    }, {
-        "key": "city",
-        "value": $cityInput.value.toUpperCase()
-    }, {
-        "key": "county",
-        "value": $countyInput.value.toUpperCase()
-    }, {
-        "key": "zip",
-        "value": $zipInput.value.toUpperCase()
-    }, {
-        "key": "schooltype",
-        "value": $schooltypeInput.value.toUpperCase()
-
-    }];
-
-    var tableData = MoDataSet;
-
-    filterArray.forEach(input => {
-        var filterKey = input.key;
-        var filterValue = input.value;
-
-        if (filterValue == "") {
-            return;
-        } else {
-            tableData = tableData.filter(school => {
-                return filterValue === school[filterKey];
-            });
-        };
-
-    });
-    filteredschooldata = tableData;
-    renderTable();
-
-}
 function popUpWindow(URL, windowName, windowWidth, windowHeight) {
     var centerLeft = (screen.width/2)-(windowWidth/2);
     var centerTop = (screen.height/2)-(windowHeight/2);
     var windowFeatures = 'toolbar=no, location=no, directories=no, status=no, menubar=no, titlebar=no, scrollbars=no, resizable=yes, ';
     return window.open(URL, windowName, windowFeatures +' width='+ windowWidth +', height='+ windowHeight +', top='+ centerTop +', left='+ centerLeft);
    }
-
